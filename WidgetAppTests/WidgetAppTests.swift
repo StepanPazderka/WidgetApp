268c12b04bf5c72ae5b227f7f41cdf4d56144da6
Stepan Pazderka <stepan.pazderka@icloud.com>
2026-04-29 22:06:53 +0200

    Tests created

---
+++ WidgetAppTests/WidgetAppTests.swift
@@ -0,0 +1,18 @@
+//
+//  WidgetAppTests.swift
+//  WidgetAppTests
+//
+//  Created by Štěpán Pazderka on 29.04.2026.
+//
+
+import Testing
+
+struct WidgetAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
